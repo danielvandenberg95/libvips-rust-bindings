493f5acf0beffbf057325dbd62d5006dd92b286a
Daniël van den Berg <danielvandenberg95@gmail.com>
2022-10-11 08:47:34 +0200

    Update generate.sh

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $(pwd):/src -v $(pwd)/../src/:/bindings -e RUSTFLAGS='-C target-feature=-crt-static' -e BINDINGS_DIR=/bindings -w /src -it libvips-builder cargo build
+docker run --rm -v $(pwd):/src -v $(pwd)/../src/:/bindings -e RUSTFLAGS='-C target-feature=-crt-static' -e BINDINGS_DIR=/bindings -w /src libvips-builder cargo build
